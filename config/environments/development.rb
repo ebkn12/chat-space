317c5e9a0be023a6a5cbbf55f632cf708f66f0e8
Ebinuma Kenichi <kenichi@ebiken-mbp.local>
2017-04-11 17:50:48 +0900

    add setting of action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -53,4 +53,15 @@ Rails.application.configure do
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttles_auto: true,
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "smtp.gmail,com",
+    user_name: "dev.kenichi@gmail.com",
+    password: "keq26ekr",
+    authentication: "login"
+  }
 end
