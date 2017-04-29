200737dd6855507b81a28e0b20d24347946d4f40
Ebinuma Kenichi <kenichi@ebiken-mbp.local>
2017-04-29 17:31:29 +0900

    add routing of messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:new, :create, :edit]
+  resources :groups, only: [:new, :create, :edit] do
+    resources :messages, only: [:index, :show]
+  end
   root "messages#index"
 end
