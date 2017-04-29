1dc3f91ac22a0d069819a89b40a686e3ea083677
Ebinuma Kenichi <kenichi@ebiken-mbp.local>
2017-04-29 18:46:10 +0900

    add open_chat method

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :open_chat
+
   def index
     @groups = Group.all
+  end
+
+  private
+  def open_chat
+    @groups = current_user.groups.all
+    @group = current_user.groups.find(params[:group_id])
   end
 end
