f939c1d71abff46b2e1ec9eacdbadb287c245676
Ebinuma Kenichi <kenichi@ebiken-mbp.local>
2017-05-03 01:14:58 +0900

    modify option of routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :groups, only: [:new, :create, :edit, :update] do
+  resources :groups, except: [:index, :show, :destroy] do
     resources :messages, only: [:index, :new]
   end
   root "messages#index"
