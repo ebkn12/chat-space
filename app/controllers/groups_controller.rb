6ad43f3a0bd15425b7a69e34293289ca1da993d3
Ebinuma Kenichi <kenichi@ebiken-mbp.local>
2017-05-03 01:20:55 +0900

    def set_group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,6 @@
 class GroupsController < ApplicationController
+  before_action :set_group, only: [:edit, :update]
+
   def new
     @group = Group.new
   end
@@ -14,11 +16,9 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    @group = Group.find(params[:id])
   end
 
   def update
-    @group = Group.find(params[:id])
     if @group.update(group_params)
       redirect_to new_group_message_path(@group), notice: "グループ編集に成功しました"
     else
@@ -30,5 +30,9 @@ class GroupsController < ApplicationController
   private
   def group_params
     params.require(:group).permit(:name, user_ids:[])
+  end
+
+  def set_group
+    @group = Group.find(params[:id])
   end
 end
