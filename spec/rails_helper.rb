a20d320bbb4036fa1aaf2b5bb5c71c653746ce3e
Ebinuma Kenichi <kenichi@ebiken-mbp.local>
2017-05-07 18:29:55 +0900

    delete devise option

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,7 +2,6 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
-require 'devise'
 
 ActiveRecord::Migration.maintain_test_schema!
 
@@ -18,7 +17,6 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
   config.include FactoryGirl::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::TestHelpers, type: :controller
 end
 
