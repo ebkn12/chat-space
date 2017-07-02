ad3bdad8a9dabd1e465b1d0f13b8ab2ce8922ebe
Ebinuma Kenichi <ktennis.mqekr12@gmail.com>
2017-07-02 21:35:49 +0900

    modify controller and model indent

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,7 +3,8 @@ class Group < ApplicationRecord
   has_many :users, through: :group_users
   has_many :messages
 
-  validates_presence_of :name, :user_ids
+  validates :name, presence: true, uniqueness: true
+  validates :user_ids, presence: true
 
   def latest_message
     messages.last.try(:body) || "No messages"
