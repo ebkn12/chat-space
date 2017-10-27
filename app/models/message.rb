a7689fab895563064cdab1038dbce8fd42555402
ebkn12 <ktennis.mqekr12@gmail.com>
2017-10-27 23:38:32 +0900

    add constrains to models

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,6 +3,7 @@ class Message < ApplicationRecord
   belongs_to :group
 
   validates_presence_of :body, unless: :image?
+  validates_presence_of :image, unless: :body?
 
   mount_uploader :image, ImageUploader
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :group_users
+  has_many :group_users, dependent: :destroy
   has_many :groups, through: :group_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   scope :get_keyword, lambda { |id, keyword|
     where.not(id: id).where('name LIKE(?)', "%#{keyword}%").order('updated_at ASC')
